a687be77df5614ad239abe469dec40f86371dafa
Anurag Roy <38952781+Anuragroyan@users.noreply.github.com>
2024-03-15 11:36:03 +0530

    Rock, Paper & Scissors Game is ready I just modify the control flow of game

--- app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
@@ -1,34 +1,42 @@
 package com.example.rockpaperscissors
 
+import androidx.core.text.isDigitsOnly
+import androidx.core.util.rangeTo
+
 fun main(){
    var computerChoice = ""
    var playerChoice = ""
     println("Rock, Paper or Scissors? Enter your choice!")
     playerChoice = readln()
-    val randomNumber = (1..3).random()
-    when (randomNumber) {
-           1 -> {
-               computerChoice = "Rock"
-           }
-           2 -> {
-               computerChoice = "Paper"
-           }
-           3 -> {
-               computerChoice = "Scissors"
-           }
+    while(playerChoice == "Rock" || playerChoice == "Paper" || playerChoice == "Scissors"){
+        val randomNumber = (1..3).random()
+        when (randomNumber) {
+            1 -> {
+                computerChoice = "Rock"
+            }
+            2 -> {
+                computerChoice = "Paper"
+            }
+            3 -> {
+                computerChoice = "Scissors"
+            }
+        }
+        println(computerChoice)
+        val winner = when {
+            playerChoice == computerChoice -> "Tie"
+            playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+            playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+            playerChoice ==  "Scissors" && computerChoice == "Paper" -> "Player"
+            else -> "Computer"
+        }
+        if(winner == "Tie"){
+            println("It's a Tie")
+            break
+        }
+        else{
+            println("$winner won!")
+            break
+        }
     }
-    println(computerChoice)
-    val winner = when {
-        playerChoice == computerChoice -> "Tie"
-        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
-        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
-        playerChoice ==  "Scissors" && computerChoice == "Paper" -> "Player"
-        else -> "Computer"
-    }
-    if(winner == "Tie"){
-        println("It's a Tie")
-    }
-    else{
-        println("$winner won!")
-    }
+    println("Please enter only rock paper scissors as input!")
 }
